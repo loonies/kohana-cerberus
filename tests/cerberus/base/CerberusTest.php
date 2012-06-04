22775362882d10c599f4150c8dfdf5cfe96a579d
Miodrag Tokić <mtokic@gmail.com>
2012-06-05 01:19:59 +0200

    Improved Cerberus tests

--- tests/cerberus/base/CerberusTest.php
+++ tests/cerberus/base/CerberusTest.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Cerberus_Exception test
+ * Cerberus test
  *
  * @group  cerberus
  * @group  cerberus.base
@@ -15,10 +15,8 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * @covers  Cerberus::__construct
-	 *
-	 * @return  void
 	 */
-	public function test_constructor_gets_storage()
+	public function test_it_sets_storage()
 	{
 		$storage = $this->getMock('Cerberus_Storage');
 
@@ -29,32 +27,8 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * @covers  Cerberus::authenticate
-	 * @expectedException  Cerberus_Exception
-	 *
-	 * @return  void
 	 */
-	public function test_authentication_failure_throws_exception()
-	{
-		$adapter = $this->getMock('Cerberus_Adapter');
-
-		$adapter
-			->expects($this->once())
-			->method('authenticate')
-			->will($this->returnValue(new Cerberus_Result(Cerberus_Result::FAILURE_GENERAL, 'itsnotme')));
-
-		$storage = $this->getMock('Cerberus_Storage');
-
-		$cerberus = new Cerberus($storage);
-
-		$cerberus->authenticate($adapter);
-	}
-
-	/**
-	 * @covers  Cerberus::authenticate
-	 *
-	 * @return  void
-	 */
-	public function test_successful_authentication_writes_to_storage()
+	public function test_it_writes_identity_to_storage_if_authentication_passes()
 	{
 		$adapter = $this->getMock('Cerberus_Adapter');
 
@@ -67,7 +41,12 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 
 		$storage
 			->expects($this->once())
-			->method('write');
+			->method('clear');
+
+		$storage
+			->expects($this->once())
+			->method('write')
+			->with($this->equalTo('itsme'));
 
 		$cerberus = new Cerberus($storage);
 
@@ -76,17 +55,51 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * @covers  Cerberus::authenticate
-	 *
-	 * @return  void
 	 */
-	public function test_successful_authentication_returns_result_obj()
+	public function test_it_does_not_writes_identity_to_storage_if_authentication_passes()
 	{
 		$adapter = $this->getMock('Cerberus_Adapter');
 
 		$adapter
 			->expects($this->once())
 			->method('authenticate')
-			->will($this->returnValue(new Cerberus_Result(Cerberus_Result::SUCCESS, 'itsme')));
+			->will($this->returnValue(new Cerberus_Result(Cerberus_Result::FAILURE_GENERAL, 'itsnotme')));
+
+		$storage = $this->getMock('Cerberus_Storage');
+
+		$storage
+			->expects($this->once())
+			->method('clear');
+
+		$storage
+			->expects($this->never())
+			->method('write');
+
+		$cerberus = new Cerberus($storage);
+
+		try
+		{
+			$cerberus->authenticate($adapter);
+
+			$this->fail('Should throw Cerberus_Exception');
+		}
+		catch(Cerberus_Exception $e)
+		{
+
+		}
+	}
+
+	/**
+	 * @covers  Cerberus::authenticate
+	 */
+	public function test_it_returns_authentication_result_if_authentication_passes()
+	{
+		$adapter = $this->getMock('Cerberus_Adapter');
+
+		$adapter
+			->expects($this->once())
+			->method('authenticate')
+			->will($this->returnValue(new Cerberus_Result(Cerberus_Result::SUCCESS, 'bar')));
 
 		$storage = $this->getMock('Cerberus_Storage');
 
@@ -96,11 +109,57 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * @covers  Cerberus::is_valid
+	 * @covers  Cerberus::authenticate
 	 *
-	 * @return  void
+	 * @expectedException      Cerberus_Exception
+	 * @expectedExceptionCode  -1
 	 */
-	public function test_is_not_valid_when_storage_empty()
+	public function test_it_throws_exception_if_authentication_fails()
+	{
+		$adapter = $this->getMock('Cerberus_Adapter');
+
+		$adapter
+			->expects($this->once())
+			->method('authenticate')
+			->will($this->returnValue(new Cerberus_Result(Cerberus_Result::FAILURE_IDENTITY_NOT_FOUND, 'itsnotme')));
+
+		$storage = $this->getMock('Cerberus_Storage');
+
+		$cerberus = new Cerberus($storage);
+
+		$cerberus->authenticate($adapter);
+	}
+
+	/**
+	 * @covers  Cerberus::identity
+	 */
+	public function test_it_returns_identity_from_successful_authentication_attempt()
+	{
+		$adapter = $this->getMock('Cerberus_Adapter');
+
+		$adapter
+			->expects($this->once())
+			->method('authenticate')
+			->will($this->returnValue(new Cerberus_Result(Cerberus_Result::SUCCESS, 'foo')));
+
+		$storage = $this->getMock('Cerberus_Storage');
+
+		$storage
+			->expects($this->once())
+			->method('read')
+			->will($this->returnValue('foo'));
+
+		$cerberus = new Cerberus($storage);
+
+		$cerberus->authenticate($adapter);
+
+		$this->assertSame('foo', $cerberus->identity());
+	}
+
+	/**
+	 * @covers  Cerberus::is_valid
+	 */
+	public function test_it_is_not_valid_when_storage_empty()
 	{
 		$storage = $this->getMock('Cerberus_Storage');
 
@@ -116,10 +175,8 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * @covers  Cerberus::is_valid
-	 *
-	 * @return  void
 	 */
-	public function test_is_valid_when_storage_not_empty()
+	public function test_it_is_valid_when_storage_not_empty()
 	{
 		$storage = $this->getMock('Cerberus_Storage');
 
@@ -135,10 +192,8 @@ class Cerberus_Base_CerberusTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * @covers  Cerberus::clear
-	 *
-	 * @return  void
 	 */
-	public function test_clear_will_clear_storage()
+	public function test_it_clears_storage()
 	{
 		$storage = $this->getMock('Cerberus_Storage');
 
