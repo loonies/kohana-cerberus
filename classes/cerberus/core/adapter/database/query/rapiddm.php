83c4191ecd48ee70cc5709816adc1c70cd8b74bd
Miodrag Tokić <mtokic@gmail.com>
2012-06-29 20:53:43 +0200

    Added Rapid Data Mapper query for the database adapter

---
+++ classes/cerberus/core/adapter/database/query/rapiddm.php
@@ -0,0 +1,54 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Rapiddm query
+ *
+ * Implements a bridge design pattern between Cerberus and Rapiddm
+ *
+ * @package    Cerberus
+ * @category   Adapter/Database
+ * @author     Miodrag Tokić <mtokic@gmail.com>
+ * @copyright  (c) 2011, Miodrag Tokić
+ * @license    New BSD License
+ */
+class Cerberus_Core_Adapter_Database_Query_Rapiddm
+	extends Cerberus_Adapter_Database_Query {
+
+	/**
+	 * @var  Db_Connection
+	 */
+	private $db;
+
+	/**
+	 * Creates a new Cerberus Rapiddm query
+	 *
+	 * @param   array   Config options
+	 * @param   Db_Connection
+	 * @return  void
+	 */
+	public function __construct(array $config = array(), Db_Connection $db)
+	{
+		parent::__construct($config);
+
+		$this->db = $db;
+	}
+
+	/**
+	 * Implements [Cerberus_Adapter_Database_Query::find]
+	 *
+	 * @see  Cerberus_Adapter_Database_Query::find
+	 *
+	 * @param   string  Identity to search for
+	 * @return  string  Password of identity or NULL in case identity not found
+	 */
+	public function find($identity)
+	{
+		$result = $this->db->select()
+			->from($this->table, $this->columns['password'])
+			->where($this->columns['identity'], $identity)
+			->limit(1)
+			->get()
+			->val();
+
+		return ($result === FALSE) ? NULL : $result;
+	}
+}
