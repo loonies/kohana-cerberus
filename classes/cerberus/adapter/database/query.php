9ff45faa907a490061abdab9c9d9cd29101c5d28
Miodrag Tokić <mtokic@gmail.com>
2012-06-29 23:57:53 +0200

    Capitalized "OR" in the file protection line

--- classes/cerberus/adapter/database/query.php
+++ classes/cerberus/adapter/database/query.php
@@ -1,3 +1,3 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 abstract class Cerberus_Adapter_Database_Query extends Cerberus_Core_Adapter_Database_Query {}
--- classes/cerberus/storage/cookie.php
+++ classes/cerberus/storage/cookie.php
@@ -1,3 +1,3 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 class Cerberus_Storage_Cookie extends Cerberus_Core_Storage_Cookie {}
